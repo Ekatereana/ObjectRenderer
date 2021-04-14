92a93f04ea002cdf45ed26a2e378c06c420f81b1
Ekatereana <ekatereanagricaenko@gmail.com>
2021-04-14 20:37:19 +0300

    clean up rendering

--- src/main/java/obj/rendering/abstracts/ObjectComponent.java
+++
@@ -1,4 +0,0 @@
-package obj.rendering.abstracts;
-
-public class ObjectComponent {
-}
--- src/main/java/obj/rendering/sceneComponents/SceneRender.java
+++
@@ -1,7 +0,0 @@
-package obj.rendering.sceneComponents;
-
-public interface SceneRender {
-
-  void render(AbstractScene scene);
-
-}
